d3e52a07beda049da7ce9ef5ca3fcb3dbed9bba1
Sunny NG <ngsanluk@gmail.com>
2017-02-26 19:11:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# iOS Reverse Geocoding Demo
+This example shows translating a CLLocation object created with latitude and longitude to a human readable text
--- ReverseGeocodingDemo/ViewController.swift
+++ ReverseGeocodingDemo/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var latLabel: UITextField!
+    @IBOutlet weak var lngLabel: UITextField!
+    @IBOutlet weak var geocodingResultTextView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func startReverseGeocoding(_ sender: Any) {
+        let location = CLLocation(latitude: Double(self.latLabel.text!)!, longitude: Double(self.lngLabel.text!)!)
+        
+        let geocoder = CLGeocoder()
+        
+        geocoder.reverseGeocodeLocation(location, completionHandler: {(placemarks, error) in
+            if error != nil {
+                self.geocodingResultTextView.text = "Error!"
+            }
+            else if placemarks!.count > 0{
+                let placemark = placemarks![0]
+                let addressDictionary = placemark.addressDictionary
+                let address = addressDictionary!["Street"]
+                let city = addressDictionary!["City"]
+                self.geocodingResultTextView.text = "\(address!), \(city!)"
+            }
+        })
+        
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
